e10873b93f5db3a8864c4d186a7513a6f26604b0
Joao <57973684+WhaleJucs@users.noreply.github.com>
2025-09-30 18:00:50 -0300

    Correções de lógica implementadas

--- src/TaxCalculator.js
+++ src/TaxCalculator.js
@@ -1,46 +1,81 @@
+import Operation from './Operation.js'
+import Tax from './Tax.js'
+
 class TaxCalculator {
+
     constructor() {
         this.saldo = 0
         this.custoMedio = 0
         this.taxResult = []
+        this.totalVendasMes = 0
+        this.valorPrejuizo = 0
     }
-}
 
-processOperation(operation) {
+    processOperation(operation) {
+       
+        // Lógica para processar a operação
+        if (operation.operation === 'buy') {
 
-    // Lógica para processar a operação
-    if (operation.operation === 'buy') {
-
-        // Atualiza saldo e custo médio para compra
-        this.saldo += operation.quantity
-        this.custoMedio = ((this.custoMedio * (this.saldo - operation.quantity))
-                             + (operation.unitCost * operation.quantity))
-                             / this.saldo
-        this.taxResult.push(0) // Sem taxa para compra
-    } 
-
-    else if (operation.operation === 'sell') {
-
-        // Verifica se há saldo suficiente para a venda
-        if (operation.quantity > this.saldo) {
-            throw new Error('Saldo insuficiente para venda')
-        }
-
-        // Atualiza saldo e custo médio para venda
-        this.saldo -= operation.quantity
-        const lucroTotal = (operation.unitCost - this.custoMedio) * operation.quantity
-
-        // Calcula o valor total da venda
-        const valorTotalVenda = operation.unitCost * operation.quantity
-
-        // Impletação da regra tributária
-        if (lucroTotal > 0 && valorTotalVenda > 20000) {
-            const tax = lucroTotal * 0.15 // 15% de imposto sobre o lucro segundo as leis brasileiras
-            this.taxResult.push(tax)
+            // Atualiza saldo e custo médio para compra
+            this.saldo += operation.quantity
+            this.custoMedio = ((this.custoMedio * (this.saldo - operation.quantity))
+                                + (operation.unitCost * operation.quantity))
+                                / this.saldo
+            this.taxResult.push(new Tax(0)) // Sem taxa para compra
         } 
 
-        else {
-            this.taxResult.push(0) // Sem taxa para venda
+        else if (operation.operation === 'sell') {
+
+            // Verifica se há saldo suficiente para a venda
+            if (operation.quantity > this.saldo) {
+                throw new Error('Saldo insuficiente para venda')
+            }
+
+            // Atualiza saldo e custo médio para venda
+            this.saldo -= operation.quantity
+            const lucroTotal = (operation.unitCost - this.custoMedio) * operation.quantity
+
+            // Calcula o valor total da venda do mês
+            const valorTotalVenda = operation.unitCost * operation.quantity
+            this.totalVendasMes += valorTotalVenda
+
+            // Impletação da regra tributária
+            if (lucroTotal > 0) {
+
+                const lucroAposPrejuizo = lucroTotal - this.valorPrejuizo // Abate o prejuízo acumulado
+
+                if (lucroAposPrejuizo <= 0) {
+                    this.valorPrejuizo = Math.abs(lucroAposPrejuizo) // Atualiza o prejuízo acumulado
+                    this.taxResult.push(new Tax(0)) // Sem taxa para prejuízo
+                }
+
+                else if (lucroAposPrejuizo > 0) {
+                    this.valorPrejuizo = 0 // Zera o prejuízo acumulado
+                    lucroTotal = lucroAposPrejuizo // Atualiza o lucro total
+
+                    if (this.totalVendasMes > 20000) {
+
+                        // Calcula o imposto sobre o lucro      
+                        const tax = lucroTotal * 0.2 // 20% de imposto sobre o lucro segundo as leis brasileiras
+                        this.taxResult.push(new Tax(tax))
+                        lucroTotal -= tax // Abate o imposto do lucro
+                    }
+
+                    else {
+                        this.taxResult.push(new Tax(0)) // Sem taxa para venda
+                    }
+                }
+            } 
+
+            else if (lucroTotal < 0) {
+
+                this.valorPrejuizo += Math.abs(lucroTotal) // Acumula o prejuízo
+                this.taxResult.push(new Tax(0)) // Sem taxa para prejuízo
+            }
+
+            else {
+                this.taxResult.push(new Tax(0)) // Sem taxa para venda
+            }
         }
     }
 }
